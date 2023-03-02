af801338d65101f9f315322d33035e901b166da7
hamza <hamzanazir719@gmail.com>
2023-03-02 14:23:05 +0500

    change file

---
+++ data&time.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+from datetime import datetime,timedelta
+
+
+# In[19]:
+
+
+first_date=datetime(2021,12,23,12,56)
+second_date=datetime(2020,11,22,11,56)
+def differ(first,second):
+    total=first-second
+    print(total)
+    
+    
+differ(first_date,second_date)
+
+
+# In[ ]:
+
+
+
+
